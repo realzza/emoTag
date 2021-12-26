b6dc69db6628c07e409fcb4094c4a8243548c8b4
BlackNinja <zz188@duke.edu>
2021-12-26 22:24:17 +0800

    add util functions

---
+++ utils/util.py
@@ -0,0 +1,67 @@
+import json
+import torch
+import random
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from itertools import repeat
+from collections import OrderedDict
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+def random_split(dataset, valid_split):
+    assert 0 <= valid_split < 1
+    valid_len = int(len(dataset) * valid_split)
+    if valid_len == 0:
+        return dataset, None
+    train_len = len(dataset) - valid_len
+    trainset, validset = torch.utils.data.random_split(dataset, [train_len, valid_len])
+    return trainset, validset
+
+def ensure_dir(dirname):
+    dirname = Path(dirname)
+    if not dirname.is_dir():
+        dirname.mkdir(parents=True, exist_ok=False)
+
+def read_json(fname):
+    fname = Path(fname)
+    with fname.open('rt') as handle:
+        return json.load(handle, object_hook=OrderedDict)
+
+def write_json(content, fname):
+    fname = Path(fname)
+    with fname.open('wt') as handle:
+        json.dump(content, handle, indent=4, sort_keys=False)
+
+def inf_loop(dataloader):
+    ''' wrapper function for endless data loader. '''
+    for loader in repeat(dataloader):
+        yield from loader
+
+class MetricTracker:
+    def __init__(self, *keys, writer=None):
+        self.writer = writer
+        self._data = pd.DataFrame(index=keys, columns=['total', 'counts', 'average'])
+        self.reset()
+        
+    def reset(self):
+        for col in self._data.columns:
+            self._data[col].values[:] = 0
+
+    def update(self, key, value, n=1):
+        if self.writer is not None:
+            self.writer.add_scalar(key, value)
+        self._data.total[key] += value * n
+        self._data.counts[key] += n
+        self._data.average[key] = self._data.total[key] / self._data.counts[key]
+
+    def avg(self, key):
+        return self._data.average[key]
+    
+    def result(self):
+        return dict(self._data.average)
